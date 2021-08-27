1ff0b617ea2b3bb16a20e4567d0d0270eb5df65f
jianghe <jianghe8316@gmail.com>
2021-08-27 10:46:14 +0700

    pod test

---
+++ Classes/MyToast.swift
@@ -0,0 +1,15 @@
+//
+//  MyToast.swift
+//  PodTest
+//
+//  Created by mac on 2021/8/26.
+//
+
+import UIKit
+
+open class MyToast: NSObject {
+
+    public static func show() {
+        puts(#function)
+    }
+}
